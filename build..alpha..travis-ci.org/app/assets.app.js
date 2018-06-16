e3c5aade4dd6ab98a90bb803f7042a27c0a8c866
Travis CI User <travis@example.org>
2018-06-16 08:03:02 +0000

    [ci skip] update gh-pages

--- build..alpha..travis-ci.org/app/assets.app.js
+++ build..alpha..travis-ci.org/app/assets.app.js
@@ -32956,7 +32956,7 @@ local.stateInit({
             "npm_package_homepage": "https://github.com/npmtest/node-npmtest-abbrev",
             "npm_package_name": "npmtest-abbrev",
             "npm_package_nameLib": "npmtest_abbrev",
-            "npm_package_version": "2017.4.27"
+            "npm_package_version": "2018.6.16"
         }
     }
 });
